5041ac4edc92dcc04e60608a4463852fa3c3665b
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-12-13 02:29:40 +0800

    chore: import React

--- src/options/components/editor.tsx
+++ src/options/components/editor.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import React, { useRef } from 'react'
 import MonacoEditor, { monaco } from 'react-monaco-editor'
 
 export interface EditorProps {
--- src/options/components/options.tsx
+++ src/options/components/options.tsx
@@ -1,4 +1,4 @@
-import { useState, useLayoutEffect } from 'react'
+import React, { useState, useLayoutEffect } from 'react'
 import { Editor } from './editor'
 import * as DAO from '@utils/dao'
 
