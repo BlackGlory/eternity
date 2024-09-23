e028de9434dc8aaa382d678956179b8e5a265baf
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2024-09-23 11:53:01 +0800

    chore: format

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -9,15 +9,15 @@ export function IconButton(props: React.ComponentPropsWithoutRef<'button'>) {
           'border w-6 h-6 inline-flex justify-center items-center'
 
           // Light
-          , 'hover:bg-gray-300 disabled:bg-gray-300'
-          , 'text-gray-700 disabled:text-white hover:text-gray-900'
+        , 'hover:bg-gray-300 disabled:bg-gray-300'
+        , 'text-gray-700 disabled:text-white hover:text-gray-900'
 
           // Dark
-          , 'dark:hover:bg-gray-700 dark:disabled:bg-gray-800'
-          , 'dark:text-white dark:disabled:text-gray-400 dark:hover:text-gray-200'
-          , 'dark:border-gray-600'
+        , 'dark:hover:bg-gray-700 dark:disabled:bg-gray-800'
+        , 'dark:text-white dark:disabled:text-gray-400 dark:hover:text-gray-200'
+        , 'dark:border-gray-600'
 
-          , props.className
+        , props.className
         )
       }
     />
