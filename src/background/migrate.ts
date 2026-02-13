7d4803e9d4f160c148a260bc1e4e296404f45c48
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2026-02-13 11:22:16 +0000

    docs: add comments

--- src/background/migrate.ts
+++ src/background/migrate.ts
@@ -1,5 +1,6 @@
 import { pipeAsync } from 'extra-utils'
 
 export async function migrate(previousVersion: string): Promise<void> {
+  // 尚无需要迁移的用户数据, 此处代码仅为占位符.
   await pipeAsync(previousVersion)
 }
